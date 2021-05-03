66823f3d40c918e252ae472b22fa68b2ff38cf0f
Colin <colinbouligand@yahoo.com>
2021-05-03 11:38:40 +0200

    correction pb flashcards cross web browser

--- src/components/FlashCard.vue
+++ src/components/FlashCard.vue
@@ -28,15 +28,53 @@ export default {
             console.log(content, column)
             console.log(this.$props.content[(this.$props.column)])
         },
-        async turnCard(event){
-            //envoie le kanji et la colonne du kanji sélectionné
-            console.log(event)
-            console.log(event.explicitOriginalTarget)
-            console.log(event.explicitOriginalTarget.dataset)
-            
-                this.$emit('turn-card', event.explicitOriginalTarget.dataset.kanji,event.explicitOriginalTarget.dataset.num)
+        async turnCard(e){
+
+             // e.target.getAttribute("data-kanji") === e.explicitOriginalTarget.dataset.kanji
+                this.$emit('turn-card', e.target.getAttribute("data-kanji"),e.target.getAttribute("data-num"))
                 this.clicked =!this.clicked
                 this.correct = !this.correct
+            //envoie le kanji et la colonne du kanji sélectionné
+            // PROBLEME EN FONCTION DES NAVIGATEURS
+            //recup le navigateur de l'utilisateur
+            //var browser;
+
+           /* var isFirefox = typeof InstallTrigger !== 'undefined';
+
+            console.log("Firefox", isFirefox)
+
+            var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
+            console.log("chrome", isChrome)*/
+
+            //var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+            //console.log("opera", isOpera)
+            //console.log(e.target.getAttribute())
+            
+                 
+               
+
+            
+           /* else 
+            {
+               console.log("event", e.srcElement) //pour opera et chrome
+                console.log(e.srcElement.data)
+                
+               // this.$emit('turn-card', e.explicitOriginalTarget.dataset.kanji,e.explicitOriginalTarget.dataset.num)
+                
+            }*/
+               
+
+
+            //console.log(navigator.userAgent)
+            //var agent = navigator.userAgent.match(/(opera|chrome|safari|firefox|msie)/?s*(.?d+(.d+)*)/i)
+            //var event=e||window.event;
+            //console.log("event", event)
+            //console.log(event.explicitOriginalTarget)
+            //console.log(event.explicitOriginalTarget.dataset)
+            
+                
             
 
         }
